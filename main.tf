2052ded7dcdea567625fb517b1fadfffdc56751d
Bharat.singh@hitachivantara.com <bharat.singh@hitachivantara.com>
2023-04-04 09:43:53 +0530

    code refactoring

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
-module "my_s3" {
-  source       = "./iac/modules/s3"
-  bucket_name  = var.bucket_name
-  event_lambda = module.my_lambda.event_lambda
+module "get_s3_notification_lambda" {
+  source = "./infra/modules/lambda"
 }
 
-module "my_lambda" {
-  source = "./iac/modules/lambda"
+module "file_upload_s3" {
+  source       = "./infra/modules/s3"
+  bucket_name  = var.bucket_name
+  event_lambda = module.get_s3_notification_lambda.event_lambda
 }
-module "cloud-watch" {
-  source        = "./iac/modules/cloudwatch"
-  function_name = module.my_lambda.event_lambda.function_name
+
+module "get_notification_cloud_watch" {
+  source        = "./infra/modules/cloudwatch"
+  function_name = module.get_s3_notification_lambda.event_lambda.function_name
 }
